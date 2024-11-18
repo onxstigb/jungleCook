0aa3eb847b455ea6b548b2b78a357e997d0dd23c
onxstigb <obrookins11@gmail.com>
2024-11-18 08:54:11 -0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+
+  devtool: "eval-source-map",
+
+  entry: "./src/app.js",
+
+  output: {
+    path: path.resolve(__dirname, "dist/app"),
+    filename: "app.js",
+  },
+
+  watch: true,
+};
