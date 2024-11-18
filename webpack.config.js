99e193537357cdfb376570a586bd1d72ec30af63
onxstigb <obrookins11@gmail.com>
2024-11-18 09:24:09 -0500

    updating package.json

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,11 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-
-  devtool: "eval-source-map",
-
-  entry: "./src/app.js",
-
+  entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist/app"),
     filename: "app.js",
   },
-
+  //   this will set a watcher on the index.js file for any changes
   watch: true,
 };
